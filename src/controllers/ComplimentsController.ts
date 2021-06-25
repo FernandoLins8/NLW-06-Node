049a8714ea27c33ce7f2051bc72faa79ffcdd369
Fernando Lins <fernandolins088@gmail.com>
2021-06-25 04:47:20 -0300

    Update ComplimentsController: retrieve user_sender id from request, instead of req.body

--- src/controllers/ComplimentsController.ts
+++ src/controllers/ComplimentsController.ts
@@ -3,15 +3,16 @@ import ComplimentsService from '../services/ComplimentsService'
 
 class ComplimentsController {
   async create(req: Request, res: Response) {
-    const { user_sender, user_receiver, tag_id, message } = req.body
+    const { user_receiver, tag_id, message } = req.body
+    const { user_id } = req
     
-    if(!user_sender || !user_receiver || !tag_id) {
-      res.json({ 'message': 'A compliment must have a sender, receiver and tag' })
+    if(!user_id || !user_receiver || !tag_id) {
+      return res.json({ 'message': 'A compliment must have a sender, receiver and tag' })
     }
     
     const complimentsService = new ComplimentsService()
     const compliment = await complimentsService.createCompliments({
-      user_sender,
+      user_sender: user_id,
       user_receiver,
       tag_id,
       message
