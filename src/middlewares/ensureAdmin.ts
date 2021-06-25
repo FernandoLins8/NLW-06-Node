ebe84a40194f6f1f35874b4bdca47c38e0049801
Fernando Lins <fernandolins088@gmail.com>
2021-06-25 03:51:17 -0300

    Fix ensureAdmin middlware

--- src/middlewares/ensureAdmin.ts
+++ src/middlewares/ensureAdmin.ts
@@ -1,7 +1,15 @@
 import { Request, Response, NextFunction } from 'express'
+import { getCustomRepository } from 'typeorm'
+import UsersRepository from '../repositories/UserRepository'
 
-export default function ensureAdmin(req: Request, res: Response, next: NextFunction) {
-  const admin = false
+export default async function ensureAdmin(req: Request, res: Response, next: NextFunction) {
+  const { user_id } = req
+
+  const usersRepository = getCustomRepository(UsersRepository)
+  
+  const user = await usersRepository.findOne(user_id)
+  
+  const { admin } = user
 
   if(admin) {
     return next()
